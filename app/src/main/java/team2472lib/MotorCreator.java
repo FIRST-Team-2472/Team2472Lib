9124b8257cacb6ca721493b8eeb11aaffff6ad38
Aedyn-Hylander <159387863+Aedyn-Hylander@users.noreply.github.com>
2026-07-06 17:42:50 -0500

    finished motor creator

--- app/src/main/java/team2472lib/MotorCreator.java
+++ app/src/main/java/team2472lib/MotorCreator.java
@@ -1,5 +1,6 @@
 package team2472lib;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -10,13 +11,14 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class MotorCreator {
-    int ID, currentLimit;
+    int ID, statorCurrentLimit, supplyCurrentLimit;
     boolean setInverted;
     MotorType motorType;
     IdleMode idleMode;
-    public MotorCreator(int ID, int currentLimit, MotorType motorType, boolean setInverted, IdleMode idleMode){
+    public MotorCreator(int ID, int statorCurrentLimit, int supplyCurrentLimit, MotorType motorType, boolean setInverted, IdleMode idleMode){
         this.ID = ID;
-        this.currentLimit = currentLimit;
+        this.statorCurrentLimit = statorCurrentLimit;
+        this.supplyCurrentLimit = supplyCurrentLimit;
         this.setInverted = setInverted;
         this.motorType = motorType;
         this.idleMode = idleMode;
@@ -25,7 +27,9 @@ public class MotorCreator {
     public SparkMax getSparkMax() {
         SparkMax motor = new SparkMax(ID, motorType);
         SparkMaxConfig config = new SparkMaxConfig();
-        config.inverted(setInverted).smartCurrentLimit(currentLimit).idleMode(idleMode);
+        config.inverted(setInverted).smartCurrentLimit(statorCurrentLimit).secondaryCurrentLimit(supplyCurrentLimit)
+            .idleMode(idleMode);
+
         motor.configure(config, SparkMax.ResetMode.kResetSafeParameters, SparkMax.PersistMode.kPersistParameters);
         return motor;
     }
@@ -33,7 +37,9 @@ public class MotorCreator {
     public SparkMax getSparkMax(SparkMax.ResetMode resetMode, SparkMax.PersistMode persistMode) {
         SparkMax motor = new SparkMax(ID, motorType);
         SparkMaxConfig config = new SparkMaxConfig();
-        config.inverted(setInverted).smartCurrentLimit(currentLimit).idleMode(idleMode);
+        config.inverted(setInverted).smartCurrentLimit(statorCurrentLimit).secondaryCurrentLimit(supplyCurrentLimit)
+            .idleMode(idleMode);
+
         motor.configure(config, resetMode, persistMode);
         return motor;
     }
@@ -43,8 +49,15 @@ public class MotorCreator {
 
         MotorOutputConfigs outputConfig = new MotorOutputConfigs();
             outputConfig.Inverted = setInverted ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
-            outputConfig.NeutralMode = NeutralModeValue(idleMode.value);
+            outputConfig.NeutralMode = NeutralModeValue.valueOf(idleMode.value);
+        
+        CurrentLimitsConfigs limitConfig = new CurrentLimitsConfigs();
+            limitConfig.StatorCurrentLimit = statorCurrentLimit;
+            limitConfig.SupplyCurrentLimit = supplyCurrentLimit;
 
+        motor.getConfigurator().apply(outputConfig);
+        motor.getConfigurator().apply(limitConfig);
+    
         return motor;
     }
 
