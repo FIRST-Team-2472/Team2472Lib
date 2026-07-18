6e4637111ce811725230f74f8c64117d946b5348
Aedyn-Hylander <159387863+Aedyn-Hylander@users.noreply.github.com>
2026-07-18 12:14:58 -0500

    teleopDriveCommand
    Cleaned up the teleOpDriveCommand and unmerged it from the code base

--- app/src/main/java/team2472lib/TeleOpDriveCommand.java
+++ app/src/main/java/team2472lib/TeleOpDriveCommand.java
@@ -7,84 +7,112 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
-
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 //import frc.robot.MotorPowerController;
 import edu.wpi.first.math.controller.PIDController;
 import frc.robot.RobotStatus;
 import frc.robot.Constants.*;
 import frc.robot.extras.AutoAim;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import org.littletonrobotics.junction.Logger;
+//import org.littletonrobotics.junction.Logger;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class TeleOpDriveCommand extends Command {
-    private final CommandSwerveDrivetrain drivetrain;
+    //private final CommandSwerveDrivetrain drivetrain;
+    private final Supplier<Pose2d> pose;
+    private final Supplier<ChassisSpeeds> speeds;
+    private final Consumer<SwerveRequest> requestConsumer;
+
     private Supplier<Double> inputX, inputY, inputTheta;
     private Supplier<Boolean> slowMode, intakeActive, aimActive;
-    private MotorPowerController yawController;
-    private boolean intakeAutoYawOverrided = false, autoAimYawOverrided = false;
+
+    private PIDController yawController;
     private SlewRateLimiter xAccelerationLimiter = new SlewRateLimiter(24, -48, 0);
     private SlewRateLimiter yAccelerationLimiter = new SlewRateLimiter(24, -48, 0);
     private SlewRateLimiter thetaAccelerationLimiter = new SlewRateLimiter(180, -225, 0);
 
+    private boolean intakeAutoYawOverrided = false, autoAimYawOverrided = false;
+    private boolean isKidMode;
+    
+    private double maxSpeed = 6;
+    private double deadband = .1;
+    private double maxRotation = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
+    private double speedMult = .3;
+    private double rotationMult = .5;
+
     private Rotation2d targetAngle = new Rotation2d();
     private AutoAim autoAim = new AutoAim();
 
-    /* What to publish over networktables for telemetry */
     private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
-
-    /* Robot swerve drive state */
     public final NetworkTable driveStateTable = inst.getTable("DriveState");
     private final StructPublisher<Pose2d> predictedPose = driveStateTable.getStructTopic("Predicted Pose", Pose2d.struct).publish();
-    private final StatusSignal<Angle> robotYawSignal;
 
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric().withDriveRequestType(SwerveModule.DriveRequestType.Velocity);
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+    .withDriveRequestType(SwerveModule.DriveRequestType.Velocity);
 
-    public TeleOpDriveCommand(CommandSwerveDrivetrain drivetrain, Supplier<Double> inputX, Supplier<Double> inputY, Supplier<Double> inputTheta,
-            Supplier<Boolean> slowMode, Supplier<Boolean> intakeActive, Supplier<Boolean> aimActive) {
-        this.drivetrain = drivetrain;
+    public TeleOpDriveCommand(
+            Subsystem drivetrain, Supplier<Pose2d> pose, 
+            Supplier<ChassisSpeeds> speeds,
+            Consumer<SwerveRequest> requestConsumer,
+            Supplier<Double> inputX, Supplier<Double> inputY, 
+            Supplier<Double> inputTheta, Supplier<Boolean> slowMode, 
+            Supplier<Boolean> intakeActive, 
+            Supplier<Boolean> aimActive, boolean isKidsMode) {
+        this.pose = pose;
+        this.speeds = speeds;
+        this.requestConsumer = requestConsumer;
         this.inputX = inputX;
         this.inputY = inputY;
         this.inputTheta = inputTheta;
         this.slowMode = slowMode;
         this.intakeActive = intakeActive;
         this.aimActive = aimActive;
+        this.isKidMode = isKidsMode;
         // TODO: tune pid.
-        yawController = new MotorPowerController(DriveConstants.K_YAW_CONTROLLER_KP, DriveConstants.K_YAW_CONTROLLER_KI,
-                DriveConstants.K_YAW_CONTROLLER_D_TIME, DriveConstants.K_YAW_CONTROLLER_TIME, DriveConstants.K_YAW_CONTROLLER_ALLOWED_ERROR,
-                DriveConstants.K_YAW_CONTROLLER_INITIAL_SENSOR_READ, DriveConstants.K_YAW_CONTROLLER_INTEGRAL_PROPORTIONAL_THRESHOLD);
-        // each subsystem used by the command must be passed into the
-        // addRequirements() method (which takes a vararg of Subsystem)
-        robotYawSignal = drivetrain.getPigeon2().getYaw();
-        addRequirements(this.drivetrain);
+        yawController = new PIDController(0.9, 0.0, 0.0);
+        addRequirements(drivetrain);
     }
 
-    /**
-     * The initial subroutine of a command. Called once when the command is initially scheduled.
-     */
+    public TeleOpDriveCommand(
+            Subsystem drivetrain, Supplier<Pose2d> pose, 
+            Supplier<ChassisSpeeds> speeds,
+            Consumer<SwerveRequest> requestConsumer,
+            Supplier<Double> inputX, Supplier<Double> inputY, 
+            Supplier<Double> inputTheta, Supplier<Boolean> slowMode, 
+            Supplier<Boolean> intakeActive, 
+            Supplier<Boolean> aimActive, boolean isKidsMode,
+            double maxSpeed, double deadband, double maxRotation, 
+            double speedMult, double rotationMult) {
+        this(drivetrain, pose, speeds, requestConsumer, 
+          inputX, inputY, inputTheta, slowMode, intakeActive,
+          aimActive, isKidsMode);
+        this.setMaxSpeed(maxSpeed).setDeadband(deadband)
+            .setMaxRotation(maxRotation).setSlowModeMult(speedMult, rotationMult);
+    }
+
     @Override
     public void initialize() {
     }
 
-    /**
-     * The main body of a command. Called repeatedly while the command is scheduled. (That is, it is called repeatedly until {@link #isFinished()}
-     * returns true.)
-     */
     @Override
     public void execute() {
-        double xSpeed = inputY.get() * (Math.abs(inputY.get()) < DriveConstants.K_DEADBAND ? 0 : 1);
-        double ySpeed = inputX.get() * (Math.abs(inputX.get()) < DriveConstants.K_DEADBAND ? 0 : 1);
-        double thetaSpeed = inputTheta.get() * (Math.abs(inputTheta.get()) < DriveConstants.K_DEADBAND ? 0 : 1);
+        double xSpeed = inputY.get() * (Math.abs(inputY.get()) < deadband ? 0 : 1);
+        double ySpeed = inputX.get() * (Math.abs(inputX.get()) < deadband ? 0 : 1);
+        double thetaSpeed = inputTheta.get() * (Math.abs(inputTheta.get()) < deadband ? 0 : 1);
 
-        xSpeed *= DriveConstants.K_MAX_SPEED * (slowMode.get() ? DriveConstants.K_SLOW_MODE_MULTIPLIER : 1.0);
-        ySpeed *= DriveConstants.K_MAX_SPEED * (slowMode.get() ? DriveConstants.K_SLOW_MODE_MULTIPLIER : 1.0);
-        thetaSpeed *= DriveConstants.K_MAX_ANGULAR_RATE * (slowMode.get() ? DriveConstants.K_SLOW_MODE_ROTATIONAL_MULTIPLIER : 1.0);
+        xSpeed *= maxSpeed * (slowMode.get() ? speedMult : 1.0);
+        ySpeed *= maxSpeed * (slowMode.get() ? speedMult : 1.0);
+        thetaSpeed *= maxRotation * (slowMode.get() ? rotationMult : 1.0);
 
         // if driver tries to yaw, they disable auto-yaw until they stop intaking and
         // start again
@@ -95,7 +123,7 @@ public class TeleOpDriveCommand extends Command {
             intakeAutoYawOverrided = false;
         }
 
-        if ((Math.abs(inputTheta.get()) > 0.1 && aimActive.get()) || RobotStatus.isKidMode) {
+        if ((Math.abs(inputTheta.get()) > 0.1 && aimActive.get()) || isKidMode) {
             autoAimYawOverrided = true;
         } else if (!aimActive.get()) {
             autoAimYawOverrided = false;
@@ -111,69 +139,71 @@ public class TeleOpDriveCommand extends Command {
             thetaSpeed = getAutoAimYawSpeed();
         }
 
-        predictedPose.set(new Pose2d(autoAim.getPredictedPosition(drivetrain.getState().Pose, drivetrain.getState().Speeds).getX(),
-                autoAim.getPredictedPosition(drivetrain.getState().Pose, drivetrain.getState().Speeds).getY(), targetAngle));
+        predictedPose.set(new Pose2d(autoAim.getPredictedPosition(pose.get(), speeds.get()).getX(),
+                autoAim.getPredictedPosition(pose.get(), speeds.get()).getY(), targetAngle));
 
-        if (RobotStatus.isKidMode) {
+        if (isKidMode) {
             xSpeed /= 10;
             ySpeed /= 10;
             thetaSpeed /= 2;
         }
 
-        drivetrain.setControl(drive.withVelocityX(xSpeed).withVelocityY(ySpeed).withRotationalRate(thetaSpeed));
+        requestConsumer.accept(drive
+                .withVelocityX(xSpeed)
+                .withVelocityY(ySpeed)
+                .withRotationalRate(thetaSpeed)
+        );
+        //Logger.recordOutput("Pigeon/Heading", robotYawSignal.refresh().getValueAsDouble());
+    }
 
-        Logger.recordOutput("Pigeon/Heading", robotYawSignal.refresh().getValueAsDouble());
+    @Override
+    public boolean isFinished() { return false; }
+
+    @Override
+    public void end(boolean interrupted) {}
+
+    public TeleOpDriveCommand setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+        return this;
+    }
+
+    public TeleOpDriveCommand setMaxRotation(double maxRotation) {
+        this.maxRotation = RotationsPerSecond.of(maxRotation).in(RadiansPerSecond);
+        return this;
+    }
+
+    public TeleOpDriveCommand setDeadband(double deadband) {
+        this.deadband = deadband;
+        return this;
+    }
+
+    public TeleOpDriveCommand setSlowModeMult(double speedMult, double rotationMult) {
+        this.speedMult = speedMult;
+        this.rotationMult = rotationMult;
+        return this;
     }
 
     private double getAutoAimYawSpeed() {
         double thetaSpeed;
         // targetAngle = new Rotation2d(autoAim.getTargetYaw(drivetrain.getState().Pose));
-        targetAngle = new Rotation2d(autoAim.getTargetYaw(autoAim.getPredictedPosition(drivetrain.getState().Pose, drivetrain.getState().Speeds)));
+        targetAngle = new Rotation2d(autoAim.getTargetYaw(autoAim.getPredictedPosition(pose.get(), speeds.get())));
         targetAngle = targetAngle.minus(Rotation2d.fromRadians(Math.PI));
 
-        Rotation2d angleError = drivetrain.getState().Pose.getRotation().minus(targetAngle);
+        Rotation2d angleError = pose.get().getRotation().minus(targetAngle);
         thetaSpeed = yawController.calculate(0, angleError.getRadians());
         return thetaSpeed;
     }
 
     private double getIntakeAutoYawSpeed(double ySpeed, double xSpeed) {
         double thetaSpeed;
-        if (Math.abs(inputX.get()) > DriveConstants.K_DEADBAND || Math.abs(inputY.get()) > DriveConstants.K_DEADBAND) {
+        if (Math.abs(inputX.get()) > deadband || Math.abs(inputY.get()) > deadband) {
             targetAngle = new Rotation2d(Math.atan2(ySpeed, xSpeed));
         } else {
-            targetAngle = drivetrain.getState().Pose.getRotation();
+            targetAngle = pose.get().getRotation();
         }
 
-        Rotation2d angleError = drivetrain.getState().Pose.getRotation().minus(targetAngle);
+        Rotation2d angleError = pose.get().getRotation().minus(targetAngle);
         thetaSpeed = yawController.calculate(0, angleError.getRadians());
         return thetaSpeed;
-    }
-
-    /**
-     * Returns whether this command has finished. Once a command finishes -- indicated by this method returning true -- the scheduler will call its
-     * {@link #end(boolean)} method.
-     *
-     * <p>
-     * Returning false will result in the command never ending automatically. It may still be cancelled manually or interrupted by another command.
-     * Hard coding this command to always return true will result in the command executing once and finishing immediately. It is recommended to use *
-     * {@link edu.wpi.first.wpilibj2.command.InstantCommand InstantCommand} for such an operation.
-     *
-     * @return whether this command has finished.
-     */
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-
-    /**
-     * The action to take when the command ends. Called when either the command finishes normally -- that is, it is called when {@link #isFinished()}
-     * returns true -- or when it is interrupted/canceled. This is where you may want to wrap up loose ends, like shutting off a motor that was being
-     * used in the command.
-     *
-     * @param interrupted
-     *            whether the command was interrupted/canceled
-     */
-    @Override
-    public void end(boolean interrupted) {
     }
 }
